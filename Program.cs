a3db10be851467a556f4199aed4ef10a2840a724
Jessica <jpanndora@gmail.com>
2025-11-08 01:50:49 -0300

    Exercicio Concluido

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string ginasta;
+double notaPartida, notaExecucao, notaFinal;
+
+Console.Write("Nome do ginasta: ");
+ginasta = Console.ReadLine()!;
+Console.Write("Nota da partida (0.0 - 10.0): ");
+notaPartida = double.Parse(Console.ReadLine()!);
+Console.Write("Nota da execução (0.0 - 10.0): ");
+notaExecucao = double.Parse(Console.ReadLine()!);
+notaFinal = (notaPartida * 0.4) + (notaExecucao * 0.6);
+Console.WriteLine($"Ginasta: {ginasta}");
+Console.WriteLine($"Nota final: {notaFinal:F2}");
